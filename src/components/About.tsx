a401131dfc61946a6e14df15117bdb54f7f6d80f
Cheruiyot-dev <allankipkoech9@gmail.com>
2024-06-18 22:11:42 +0300

    Fix about me content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="lg:flex-grow md:w-1/2 lg:pr-20 md:pr-16 flex flex-col md:items-start md;text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hello, I am Allan Cheruiyot.
-            <br className="hidden lg:inline-block" />a backend develope 
+            <br className="hidden lg:inline-block" />a backend developer 
           </h1>
           <p className="mb-8 leading-relaxed">
           I specialize in building robust and scalable web applications.
